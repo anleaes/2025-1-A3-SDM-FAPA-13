ccb4e187a6a1b1026ddb82a420526b08d04ee241
Nicolas <nicolasuliana10@gmail.com>
2025-06-13 01:21:24 -0300

    feat: cria model Condominio com campos básicos

--- condominiosapp/apps/condominio/models.py
+++ condominiosapp/apps/condominio/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class Condominio(models.Model):
+    nome = models.CharField('Nome', max_length=100)
+    endereco = models.TextField('Endereço')
+    cnpj = models.CharField('CNPJ', max_length=18)
+    quantidade_blocos = models.IntegerField('Quantidade de blocos')
