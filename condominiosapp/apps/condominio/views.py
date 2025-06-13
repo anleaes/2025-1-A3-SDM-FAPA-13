2074ae9973840db2ac400d1b8b727e7c969fe5b6
Nicolas <nicolasuliana10@gmail.com>
2025-06-13 02:29:04 -0300

    feat(view): implementa ViewSet para o modelo Condomínio

--- condominiosapp/apps/condominio/views.py
+++ condominiosapp/apps/condominio/views.py
@@ -2,3 +2,6 @@ from .models import Condominio
 from rest_framework import viewsets
 from .serializer import CondominioSerializer
 
+class CondominioViewSet(viewsets.ModelViewSet):
+    queryset = Condominio.objects.all()
+    serializer_class = CondominioSerializer 
