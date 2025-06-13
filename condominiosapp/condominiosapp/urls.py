801a3bae387a913f7026d49aac4d9141a832f76b
Nicolas <nicolasuliana10@gmail.com>
2025-06-13 03:52:17 -0300

    chore(urls): incluir namespace nas rotas do app condominio

--- condominiosapp/condominiosapp/urls.py
+++ condominiosapp/condominiosapp/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('condominio/', include('condominio.urls'))
+    path('condominio/', include('condominio.urls', namespace='condominio'))
 ]
