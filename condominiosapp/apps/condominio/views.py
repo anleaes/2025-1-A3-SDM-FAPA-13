8e9c91443a4c8432cd047728a7fc7d0262f74cf9
Nicolas <nicolasuliana10@gmail.com>
2025-06-22 01:33:02 -0300

    Finalize search implementation for Condominio

--- condominiosapp/apps/condominio/views.py
+++ condominiosapp/apps/condominio/views.py
@@ -1,7 +1,10 @@
 from .models import Condominio
 from rest_framework import viewsets
 from .serializer import CondominioSerializer
+from django_filters.rest_framework import DjangoFilterBackend
 
 class CondominioViewSet(viewsets.ModelViewSet):
     queryset = Condominio.objects.all()
-    serializer_class = CondominioSerializer 
+    serializer_class = CondominioSerializer
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['nome', 'cnpj']
