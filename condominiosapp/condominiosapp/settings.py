c42f383cd8b1a34fae37abb6eb9dd46dd693d4e5
Nicolas <nicolasuliana10@gmail.com>
2025-06-23 00:00:54 -0300

    feat: Register 'blocos' in INSTALLED_APPS

--- condominiosapp/condominiosapp/settings.py
+++ condominiosapp/condominiosapp/settings.py
@@ -43,7 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'condominio.apps.CondominioConfig',
-    'products.apps.ProductsConfig',
+    'blocos.apps.BlocosConfig',
     'django_filters',
     'rest_framework.authtoken',
 ]
