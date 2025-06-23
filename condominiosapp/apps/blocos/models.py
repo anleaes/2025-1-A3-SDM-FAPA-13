013cd869b23070094e89d22209572d9863423b20
Nicolas <nicolasuliana10@gmail.com>
2025-06-23 00:11:09 -0300

    feat: add Bloco model with fields name, number, apartment quantity and relation to Condominio

--- condominiosapp/apps/blocos/models.py
+++ condominiosapp/apps/blocos/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from condominio.models import Condominio
 
 # Create your models here.
+
+class Bloco(models.Model):
+    nome = models.CharField(max_length=50)
+    numero = models.IntegerField()
+    qtd_apartamentos = models.IntegerField()
+    condominio = models.ForeignKey(Condominio, on_delete=models.CASCADE, related_name='blocos')
+
+    class Meta:
+        verbose_name = 'Bloco'
+        verbose_name_plural = 'Blocos'
+        ordering = ['id']
+
+    def __str__(self):
+        return f"{self.nome} - Bloco {self.numero}"
