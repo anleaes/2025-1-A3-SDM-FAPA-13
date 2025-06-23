6a0c8efce6997c94fdc33163d93b50f6a33c9e38
Nicolas <nicolasuliana10@gmail.com>
2025-06-22 23:37:49 -0300

    feat: create 'blocos' app with base files, register in INSTALLED_APPS and set verbose_name

--- condominiosapp/condominiosapp/settings.py
+++ condominiosapp/condominiosapp/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'condominio.apps.CondominioConfig',
+    'products.apps.ProductsConfig',
     'django_filters',
     'rest_framework.authtoken',
 ]
