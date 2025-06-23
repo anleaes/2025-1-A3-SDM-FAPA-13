49d4b1b6bf022d38a56d3517bf2bb758bf046292
Nicolas <nicolasuliana10@gmail.com>
2025-06-23 00:37:57 -0300

    feat: create Bloco ViewSet to expose API endpoints

--- condominiosapp/apps/blocos/views.py
+++ condominiosapp/apps/blocos/views.py
@@ -3,6 +3,7 @@ from .models import Bloco
 from rest_framework import viewsets
 from .serializer import BlocoSerializer
 
+
 class BlocoViewSet(viewsets.ModelViewSet):
     queryset = Bloco.objects.all()
     serializer_class = BlocoSerializer
