c2c4312b9aa04d4e52155245eb5ebb6898bbb4d6
Nicolas <nicolasuliana10@gmail.com>
2025-06-24 19:08:39 -0300

    chore(funcionarios): import DjangoFilterBackend for Funcionario filtering

--- condominiosapp/apps/funcionarios/views.py
+++ condominiosapp/apps/funcionarios/views.py
@@ -2,6 +2,10 @@ from .models import Funcionario
 from rest_framework import viewsets
 from .serializer import FuncionarioSerializer
 
+# Filter
+from django_filters.rest_framework import DjangoFilterBackend
+
+
 class FuncionarioViewSet(viewsets.ModelViewSet):
     queryset = Funcionario.objects.all()
     serializer_class = FuncionarioSerializer
