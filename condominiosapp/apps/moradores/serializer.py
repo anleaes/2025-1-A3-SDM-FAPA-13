104e1e2f264b1b55e3c01dd8037365431d32e169
Nicolas <nicolasuliana10@gmail.com>
2025-06-24 16:00:37 -0300

    feat(moradores): finalize Morador serializer implementation

--- condominiosapp/apps/moradores/serializer.py
+++ condominiosapp/apps/moradores/serializer.py
@@ -2,4 +2,6 @@ from .models import Morador
 from rest_framework import serializers
 
 class MoradorSerializer(serializers.ModelSerializer):
-    pass
+    class Meta:
+        model = Morador
+        fields = '__all__'
