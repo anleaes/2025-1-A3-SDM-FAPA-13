70ed42bf5647a3e970bab3003ab9265dc3c27879
Nicolas <nicolasuliana10@gmail.com>
2025-06-23 21:10:25 -0300

    feat(apartamentos): finalize Apartamento views implementation

--- condominiosapp/apps/apartamentos/views.py
+++ condominiosapp/apps/apartamentos/views.py
@@ -4,4 +4,5 @@ from rest_framework import viewsets
 from .serializer import  ApartamentoSerializer
 
 class ApartamentoViewSet(viewsets.ModelViewSet):
-    
+    queryset = Apartamento.objects.all()
+    serializer_class = ApartamentoSerializer  
