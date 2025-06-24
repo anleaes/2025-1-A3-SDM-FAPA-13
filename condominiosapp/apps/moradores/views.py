03ef3125b3ec4f48e5d457ee1f121722026e9dfc
Nicolas <nicolasuliana10@gmail.com>
2025-06-24 16:34:53 -0300

    chore(moradores): import DjangoFilterBackend for Morador filtering

--- condominiosapp/apps/moradores/views.py
+++ condominiosapp/apps/moradores/views.py
@@ -2,6 +2,9 @@ from .models import Morador
 from rest_framework import viewsets
 from .serializer import MoradorSerializer
 
+# Filter
+from django_filters.rest_framework import DjangoFilterBackend
+
 
 class MoradorViewSet(viewsets.ModelViewSet):
     queryset = Morador.objects.all()
