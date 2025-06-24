a81453b86cf9a98d432cc9a092020973ff5cbd5f
Nicolas <nicolasuliana10@gmail.com>
2025-06-24 17:54:41 -0300

    feat(funcionarios): add initial structure for Funcionario model

--- condominiosapp/apps/funcionarios/models.py
+++ condominiosapp/apps/funcionarios/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from condominio.models import Condominio
 
-# Create your models here.
+class Funcionario(models.Model):
+    nome = models.CharField(max_length=100)
+    funcao = models.CharField(max_length=50)
+    salario = models.DecimalField(max_digits=8, decimal_places=2)
+    turno = models.CharField(max_length=20)
+    condominio = models.ForeignKey(Condominio, on_delete=models.CASCADE, related_name='funcionarios')
+
+    def __str__(self):
+        return f"{self.nome} - {self.funcao} - {self.condominio.nome}"
