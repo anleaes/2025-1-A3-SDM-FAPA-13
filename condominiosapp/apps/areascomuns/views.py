e71fe539cdcb9e2ed8a2f5512c41477707c23fa9
Nicolas <nicolasuliana10@gmail.com>
2025-06-24 20:48:11 -0300

    chore(areascomuns): import TokenAuthentication and IsAuthenticated for view protection

--- condominiosapp/apps/areascomuns/views.py
+++ condominiosapp/apps/areascomuns/views.py
@@ -5,6 +5,11 @@ from .serializer import AreaComumSerializer
 # Filter
 from django_filters.rest_framework import DjangoFilterBackend
 
+# Token
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
+
+
 class AreaComumViewSet(viewsets.ModelViewSet):
     queryset = AreaComum.objects.all()
     serializer_class = AreaComumSerializer
