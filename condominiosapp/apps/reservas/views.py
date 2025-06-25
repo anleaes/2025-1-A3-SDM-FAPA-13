02de8406f46e01c08eab69a09c8fe915a36ddec1
Nicolas <nicolasuliana10@gmail.com>
2025-06-24 23:00:47 -0300

    chore(reservas): import DjangoFilterBackend for ReservaAreaComum filtering

--- condominiosapp/apps/reservas/views.py
+++ condominiosapp/apps/reservas/views.py
@@ -2,6 +2,8 @@ from .models import ReservaAreaComum
 from rest_framework import viewsets
 from .serializer import ReservaAreaComumSerializer
 
+# Filter
+from django_filters.rest_framework import DjangoFilterBackend
 
 class ReservaAreaComumViewSet(viewsets.ModelViewSet):
     queryset = ReservaAreaComum.objects.all()
