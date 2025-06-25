80fa60ae6f28dffafc520b4d8b610dce19cc531b
Nicolas <nicolasuliana10@gmail.com>
2025-06-25 01:00:01 -0300

    feat(manutencoes): add filtering by description, date, status, and condominium to ManutencaoViewSet

--- condominiosapp/apps/manutencoes/views.py
+++ condominiosapp/apps/manutencoes/views.py
@@ -8,3 +8,5 @@ from django_filters.rest_framework import DjangoFilterBackend
 class ManutencaoViewSet(viewsets.ModelViewSet):
     queryset = Manutencao.objects.all()
     serializer_class = ManutencaoSerializer
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['descricao', 'data', 'status', 'condominio']
