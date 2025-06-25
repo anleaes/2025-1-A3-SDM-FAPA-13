4bb796cfe2909755c9ab9de830df5af0281fe9ca
Nicolas <nicolasuliana10@gmail.com>
2025-06-24 23:12:04 -0300

    chore(reservas): import TokenAuthentication and IsAuthenticated for ReservaAreaComum protection

--- condominiosapp/apps/reservas/views.py
+++ condominiosapp/apps/reservas/views.py
@@ -5,8 +5,13 @@ from .serializer import ReservaAreaComumSerializer
 # Filter
 from django_filters.rest_framework import DjangoFilterBackend
 
+# Token
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
+
 class ReservaAreaComumViewSet(viewsets.ModelViewSet):
     queryset = ReservaAreaComum.objects.all()
     serializer_class = ReservaAreaComumSerializer
     filter_backends = [DjangoFilterBackend]
     filterset_fields = ['data_reserva', 'status', 'morador', 'area_comum']
+    
