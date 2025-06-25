908576907b7cd744c92eb2c78f42d68f30c213b9
Nicolas <nicolasuliana10@gmail.com>
2025-06-24 23:58:33 -0300

    chore(eventos): import DjangoFilterBackend for Evento filtering

--- condominiosapp/apps/eventos/views.py
+++ condominiosapp/apps/eventos/views.py
@@ -2,6 +2,8 @@ from .models import Evento
 from rest_framework import viewsets
 from .serializer import EventoSerializer
 
+# Filter
+from django_filters.rest_framework import DjangoFilterBackend
 
 class EventoViewSet(viewsets.ModelViewSet):
     queryset = Evento.objects.all()
