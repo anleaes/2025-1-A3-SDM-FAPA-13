77bb4020fcbb3e1b080c63c522e42a5839c32c7b
Nicolas <nicolasuliana10@gmail.com>
2025-06-24 22:29:07 -0300

    feat(reservas): add initial structure for ReservaAreaComum model

--- condominiosapp/apps/reservas/models.py
+++ condominiosapp/apps/reservas/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from moradores.models import Morador
+from areascomuns.models import AreaComum
 
-# Create your models here.
+class ReservaAreaComum(models.Model):
+    data_reserva = models.DateField()
+    status = models.CharField(max_length=20)
+    morador = models.ForeignKey(Morador, on_delete=models.CASCADE, related_name='reservas')
+    area_comum = models.ForeignKey(AreaComum, on_delete=models.CASCADE, related_name='reservas')
+
+    def __str__(self):
+        return f"Reserva {self.area_comum.nome} - {self.data_reserva} ({self.morador.nome})"
