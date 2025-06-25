c4c1c253b23eb1438420b457e35f3e086e66d92f
Nicolas <nicolasuliana10@gmail.com>
2025-06-25 00:59:37 -0300

    chore(manutencoes): import DjangoFilterBackend for Manutencao filtering

--- condominiosapp/apps/manutencoes/views.py
+++ condominiosapp/apps/manutencoes/views.py
@@ -2,6 +2,9 @@ from .models import Manutencao
 from rest_framework import viewsets
 from .serializer import ManutencaoSerializer
 
+# Filter
+from django_filters.rest_framework import DjangoFilterBackend
+
 class ManutencaoViewSet(viewsets.ModelViewSet):
     queryset = Manutencao.objects.all()
     serializer_class = ManutencaoSerializer
