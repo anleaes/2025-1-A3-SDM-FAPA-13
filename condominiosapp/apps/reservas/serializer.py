b54fb9143bcdbd192f76bc31296afa95bb3978d4
Nicolas <nicolasuliana10@gmail.com>
2025-06-24 22:34:38 -0300

    feat(reservas): finalize ReservaAreaComum serializer implementation

--- condominiosapp/apps/reservas/serializer.py
+++ condominiosapp/apps/reservas/serializer.py
@@ -2,4 +2,6 @@ from .models import ReservaAreaComum
 from rest_framework import serializers
 
 class ReservaAreaComumSerializer(serializers.ModelSerializer):
-    pass
+    class Meta:
+        model = ReservaAreaComum
+        fields = '__all__'
